e8a2813e6f2e72b1aab9df7232c8e9f6981e9a9a
Georgy Baranov <floridavbv@gmail.com>
2019-03-30 22:05:51 -0600

    app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,26 @@ import React, { Component } from 'react';
 import SearchBar from './components/SearchBar';
 
 class App extends Component {
+    constructor() {
+        super();
 
+        this.state = {
+            gifs: [
+                {
+                    id: 1,
+                    url: 'http://fakeimg.pl/300/'
+                },
+                {
+                    id: 2,
+                    url: 'http://fakeimg.pl/300/'
+                },
+                {
+                id: 3,
+                url: 'http://fakeimg.pl/300/'
+                }
+            ]
+        }
+    }
     handleTermChange(term) {
         console.log(term);
     }
